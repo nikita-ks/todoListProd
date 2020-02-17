40048609761328e1232f60d74aa6b265def1eafc
NikitaKS <pancxxx2@gmail.com>
2020-02-17 23:01:35 +0100

    Updates

---
+++ precache-manifest.961e4579a97ad21e46f4f2b04ead4e0e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce6922d879dbe5c5a0dfdb66b00e54f",
+    "url": "/todoListProd/index.html"
+  },
+  {
+    "revision": "b2c14dab177754f619ea",
+    "url": "/todoListProd/static/css/main.910b5375.chunk.css"
+  },
+  {
+    "revision": "369b997faeaf6b6218b3",
+    "url": "/todoListProd/static/js/2.4b01e0f0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todoListProd/static/js/2.4b01e0f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2c14dab177754f619ea",
+    "url": "/todoListProd/static/js/main.12c9034e.chunk.js"
+  },
+  {
+    "revision": "dcbf6e7dc87ac46ca3fe",
+    "url": "/todoListProd/static/js/runtime-main.88ca66d0.js"
+  },
+  {
+    "revision": "1cb1f763db573cbcd8dc758bc388a2a5",
+    "url": "/todoListProd/static/media/Bebas-Regular.1cb1f763.ttf"
+  },
+  {
+    "revision": "3ea1d7ae43573efaf16ec6a8cc5ace13",
+    "url": "/todoListProd/static/media/Bebas-Regular.3ea1d7ae.eot"
+  },
+  {
+    "revision": "441f568dce30a9585f4e4e979dd4daf8",
+    "url": "/todoListProd/static/media/Bebas-Regular.441f568d.woff"
+  },
+  {
+    "revision": "a602000706152fabd139253247844b55",
+    "url": "/todoListProd/static/media/Bebas-Regular.a6020007.woff2"
+  },
+  {
+    "revision": "ec4bea1c6074b1025eba568dadd839df",
+    "url": "/todoListProd/static/media/Bebas-Regular.ec4bea1c.otf"
+  },
+  {
+    "revision": "f2e8a57571179361e6503765dce98c90",
+    "url": "/todoListProd/static/media/Bebas-Regular.f2e8a575.svg"
+  },
+  {
+    "revision": "0e0f3f2f2cb7fd99a9afd332983d1e0a",
+    "url": "/todoListProd/static/media/BebasNeue-Book.0e0f3f2f.ttf"
+  },
+  {
+    "revision": "2594d6375c3e55ec7e5f384803cbf51e",
+    "url": "/todoListProd/static/media/BebasNeue-Light.2594d637.ttf"
+  }
+]);
